87ee5da0cd0168522d1a32ae293b60a8c25c91fa
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-06-22 14:54:31 -0600

    Gem tool initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in johnson_copy_right.gemspec
+gemspec
---
+++ lib/johnson_copy_right.rb
@@ -0,0 +1,6 @@
+require "johnson_copy_right/version"
+require "johnson_copy_right/renderer"
+
+module JohnsonCopyRight
+  # Your code goes here...
+end
